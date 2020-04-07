d4ad13da3c803d82368fb30b59468ffc2c5c689c
satoshi.marumoto <satoshi.marumoto@st-ventures.jp>
2020-04-07 18:26:27 +0900

    連絡先アプリに連絡先を追加

--- ContactsSample/ViewController.swift
+++ ContactsSample/ViewController.swift
@@ -7,14 +7,74 @@
 //
 
 import UIKit
+import Contacts
 
 class ViewController: UIViewController {
 
+    let contactStore = CNContactStore()
+    let request = CNSaveRequest()
+    let contact = CNMutableContact()
+    let status = CNContactStore.authorizationStatus(for: CNEntityType.contacts)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    switch status {
+    case .notDetermined, .restricted:
+        contactStore.requestAccess(for: .contacts) { granted, error in
+            if let error = error {
+                print(error)
+            }
+            if granted {
+                registerContactApp()
+            } else {
+                // アラートからアクセスの許可をしてもらえなかった時
+            }
+        }
+    case .denied:
+        // 拒否されている場合
+    case .authorized:
+        // すでにアクセスが許可されている場合
+    default:
+        // それ以外の場合
+    }
+    
+    func registerContactApp() {
+        
+        contact.givenName = "名前"
+        contact.familyName = "名字"
+        contact.phoneticGivenName = "なまえ"
+        contact.phoneticFamilyName = "みょうじ"
+        contact.organizationName = "会社名"
+        contact.phoneticOrganizationName = "かいしゃめい"
+        contact.phoneNumbers = [
+            CNLabeledValue<CNPhoneNumber>(label: CNLabelPhoneNumberMain, value: CNPhoneNumber(stringValue: "123-4567-8910")),
+            CNLabeledValue<CNPhoneNumber>(label: "カスタムのラベル", value: CNPhoneNumber(stringValue: "123-4567-8910"))
+        ]
+        contact.emailAddresses = [
+            CNLabeledValue<NSString>(label: CNLabelHome, value: NSString(string: "sample@sample.com")),
+            CNLabeledValue<NSString>(label: CNLabelWork, value: NSString(string: "sample@sample.com"))
+        ]
+        contact.urlAddresses = [CNLabeledValue<NSString>(label: CNLabelURLAddressHomePage, value: NSString(string: "https://www.apple.com/jp/"))]
+        let address = CNMutablePostalAddress()
+        address.country = "日本"
+        address.postalCode = "123-4567"
+        address.state = "〇〇県"
+        address.city = "〇〇市"
+        address.street = "〇〇区〇〇丁目1−1"
+        
+        contact.postalAddresses = [CNLabeledValue<CNPostalAddress>(label: CNLabelHome, value: address)]
+        
+        request.add(contact, toContainerWithIdentifier: contactStore.defaultContainerIdentifier())
+        
+        do {
+            try contactStore.execute(request)
+        } catch {
+            print(error)
+        }
     }
 
 
 }
-
